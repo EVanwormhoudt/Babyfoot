d216958cd8472117a99a389fcb7db92bdeca9925
elliott <elliott.vanwormhoudt@student.junia.com>
2026-03-10 22:55:24 +0100

    feat: update `active` field in Player model to use boolean type to match with db type

--- backend/db/models.py
+++ backend/db/models.py
@@ -15,7 +15,7 @@ class Player(SQLModel, table=True):
         id (int): Unique identifier for the player.
         player_name (str): Name of the player.
         player_color (str): Color associated with the player.
-        active (int): Indicates whether the player is active.
+        active (bool): Indicates whether the player is active.
         rating_history (List[PlayerRatingHistory]): Historical ratings of the player.
         rating (CurrentPlayerRank): Current ranking of the player.
     """
@@ -23,7 +23,7 @@ class Player(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     player_name: str
     player_color: str
-    active: int
+    active: bool
     rating_history: Optional[List["PlayerRatingHistory"]] = Relationship(back_populates="player")
     rating: Optional["CurrentPlayerRank"] = Relationship(back_populates="player",
                                                          sa_relationship_kwargs={"uselist": False})
